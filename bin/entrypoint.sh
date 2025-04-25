7ba17e7191aa2d4b128430e3f54b022cde11fe96
Alejandro López <allopezcorrea@gmail.com>
2025-04-25 16:44:33 -0500

    Add dockerfile and docker compose file (#8)
    * Add dockerfile and docker compose file
    
    * revert db env variables
    
    * changed " for '
    
    * fix env env variables on entrypoint

---
+++ bin/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+until nc -z "$DATABASE_HOST" "$DATABASE_PORT"; do
+  sleep 0.5
+done
+
+bundle check || bundle install
+
+RAILS_ENV=${RAILS_ENV} bundle exec rake db:create
+RAILS_ENV=${RAILS_ENV} bundle exec rake db:migrate
+
+RAILS_ENV=${RAILS_ENV} bundle exec rails s -b 0.0.0.0
