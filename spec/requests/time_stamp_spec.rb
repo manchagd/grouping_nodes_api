7fe3940b2b1199266774de83c990ba83386bb8d4
tutto2 <tuto.2@hotmail.com>
2025-04-25 13:30:03 -0500

    Add test functionality: current_time presence and accurate format

--- spec/requests/time_stamp_spec.rb
+++ spec/requests/time_stamp_spec.rb
@@ -4,6 +4,20 @@ require 'rails_helper'
 
 RSpec.describe "TimeStamp", type: :request do
   describe "GET /time_stamp" do
-    # pending "add some examples (or delete) #{__FILE__}"
+    it "Returns the current time" do
+      get "/time_stamp"
+      expect(response).to have_http_status(:success)
+  
+      json = JSON.parse(response.body)
+      expect(json).to have_key("current_time")
+    end
+
+    it "Date and time are in accurate format" do
+      get "/time_stamp"
+      expect(response).to have_http_status(:success)
+  
+      json = JSON.parse(response.body)
+      expect(json["current_time"]).to match(/\d{2}, \w{3} \d{4}; \d{2}:\d{2}/)
+    end
   end
 end
