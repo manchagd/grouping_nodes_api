9028dddb337921e99fad704ae79510ac903774de
tutto2 <tuto.2@hotmail.com>
2025-04-30 16:22:41 -0500

    Changed the structure of the JSON response in the DateTime controller to comply with RESTful requirements. Updated the corresponding RSpec test accordingly

--- app/controllers/datetimes_controller.rb
+++ app/controllers/datetimes_controller.rb
@@ -2,6 +2,13 @@
 
 class DatetimesController < ApplicationController
   def show
-    render json: { current_time: Time.current.strftime("%d, %b %Y; %H:%M") }
+    render json: {
+      data: {
+        type: "date_and_time",
+        attributes: {
+          value: Time.current.strftime("%d, %b %Y; %H:%M")
+        }
+      }
+    }
   end
 end
--- spec/requests/datetime_spec.rb
+++ spec/requests/datetime_spec.rb
@@ -4,25 +4,24 @@ require 'rails_helper'
 
 RSpec.describe "Datetime", type: :request do
   describe "GET /datetime" do
-    it 'Returns a successful response' do
-      get "/datetime"
+    let(:parsed_response) { JSON.parse(response.body) }
+
+    before { get "/datetime" }
+
+    it 'returns a successful response' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "Returns the current time" do
-      get "/datetime"
-      expect(response).to have_http_status(:success)
-
-      json = JSON.parse(response.body)
-      expect(json).to have_key("current_time")
+    it "returns the correct structure" do
+      expect(parsed_response).to have_key("data")
+      expect(parsed_response["data"]).to include("type" => "date_and_time")
+      expect(parsed_response["data"]).to have_key("attributes")
+      expect(parsed_response["data"]["attributes"]).to have_key("value")
     end
 
-    it "Date and time are in accurate format" do
-      get "/datetime"
-      expect(response).to have_http_status(:success)
-
-      json = JSON.parse(response.body)
-      expect(json["current_time"]).to match(/\d{2}, \w{3} \d{4}; \d{2}:\d{2}/)
+    it "returns the date and time in the expected format" do
+      value = parsed_response["data"]["attributes"]["value"]
+      expect(value).to match(/\d{2}, \w{3} \d{4}; \d{2}:\d{2}/)
     end
   end
 end
