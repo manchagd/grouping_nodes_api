55f652398b64931d287e9a29615c3208439a7d7c
Daniel Garcia Diaz <mancha0703@gmail.com>
2025-04-30 17:48:55 -0500

    Update datetimes_controller.rb

--- app/controllers/datetimes_controller.rb
+++ app/controllers/datetimes_controller.rb
@@ -3,12 +3,7 @@
 class DatetimesController < ApplicationController
   def show
     render json: {
-      data: {
-        type: "date_and_time",
-        attributes: {
-          value: Time.current.strftime("%d, %b %Y; %H:%M")
-        }
-      }
+      datetime: Time.current.strftime("%d, %b %Y; %H:%M")
     }
   end
 end
