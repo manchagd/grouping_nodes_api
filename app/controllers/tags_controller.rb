ad7e852f2c61529746dc9cc7780890714cbddb6b
tutto2 <tuto.2@hotmail.com>
2025-06-03 22:10:30 -0500

    Add Blueprinter serialization and error handling to TagsController

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,21 +3,28 @@ class TagsController < ApplicationController
 
   def index
     @tags = Tag.all
+    render json: TagBlueprint.render(@tags)
   end
 
   def show
+    render json: TagBlueprint.render(@tag)
   end
 
   def create
-    tag = Tag.create(tag_params)
-
-    redirect_to tags_path
+    tag = Tag.new(tag_params)
+    if tag.save
+      render json: TagBlueprint.render(tag), status: :created
+    else
+      render json: { errors: tag.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
-    @tag.update(tag_params)
-
-    redirect_to tag_path(@tag)
+    if @tag.update(tag_params)
+      render json: TagBlueprint.render(@tag)
+    else
+      render json: { errors: @tag.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
