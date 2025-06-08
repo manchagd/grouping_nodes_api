0bc4bfeb95ead71c958fda8e9e682541c00a7ce9
tutto2 <tuto.2@hotmail.com>
2025-06-08 18:13:39 -0500

    category_blueprint_spec refactorization for test optimization

--- spec/serializers/category_blueprint_spec.rb
+++ spec/serializers/category_blueprint_spec.rb
@@ -7,43 +7,49 @@ RSpec.describe CategoryBlueprint do
     let(:category) { create(:category) }
 
     context 'and basic view is requested' do
-      let(:basic_view) { JSON.parse(CategoryBlueprint.render(category, view: :basic)) }
+      subject { JSON.parse(CategoryBlueprint.render(category, view: :basic)) }
 
       it 'only rendes id and name' do
-        expect(basic_view['name']).to be_present
-        expect(basic_view['id']).to be_present
+        is_expected.to match({
+          'id' => category.id,
+          'name' => category.name
+        })
       end
 
       it 'does not render parent_id' do
-        expect(basic_view).not_to have_key('parent_id')
+        is_expected.not_to have_key('parent_id')
       end
     end
 
     context 'and extended view is requested' do
-      let(:extended_view) { JSON.parse(CategoryBlueprint.render(category, view: :extended)) }
+      subject { JSON.parse(CategoryBlueprint.render(category, view: :extended)) }
 
       it 'rendes id, name and parent' do
-        expect(extended_view['name']).to be_present
-        expect(extended_view['id']).to be_present
-        expect(extended_view['parent']).to be_nil
+        is_expected.to match({
+          'id' => category.id,
+          'name' => category.name,
+          'parent' => category.parent
+        })
       end
 
       it 'does not render a parent_id' do
-        expect(extended_view).not_to have_key('parent_id')
+        is_expected.not_to have_key('parent_id')
       end
     end
 
     context 'and no view is specified' do
-      let(:no_specific_view) { JSON.parse(CategoryBlueprint.render(category)) }
+      subject { JSON.parse(CategoryBlueprint.render(category)) }
 
       it 'rendes id, name and parent_id' do
-        expect(no_specific_view['name']).to be_present
-        expect(no_specific_view['id']).to be_present
-        expect(no_specific_view['parent_id']).to be_nil
+        is_expected.to match({
+          'id' => category.id,
+          'name' => category.name,
+          'parent_id' => category.parent_id
+        })
       end
 
       it 'does not render a parent' do
-        expect(no_specific_view).not_to have_key('parent')
+        is_expected.not_to have_key('parent')
       end
     end
   end
@@ -52,49 +58,57 @@ RSpec.describe CategoryBlueprint do
     let(:category) { create(:category, :with_parent) }
 
     context 'and basic view is requested' do
-      let(:basic_view) { JSON.parse(CategoryBlueprint.render(category, view: :basic)) }
+      subject { JSON.parse(CategoryBlueprint.render(category, view: :basic)) }
 
       it 'only rendes id and name' do
-        expect(basic_view['name']).to be_present
-        expect(basic_view['id']).to be_present
+        is_expected.to match({
+          'id' => category.id,
+          'name' => category.name
+        })
       end
 
       it 'does not render parent_id' do
-        expect(basic_view).not_to have_key('parent_id')
+        is_expected.not_to have_key('parent_id')
       end
     end
 
     context 'and extended view is requested' do
-      let(:extended_view) { JSON.parse(CategoryBlueprint.render(category, view: :extended)) }
+      subject { JSON.parse(CategoryBlueprint.render(category, view: :extended)) }
 
       it 'rendes id, name and parent' do
-        expect(extended_view['name']).to be_present
-        expect(extended_view['id']).to be_present
-        expect(extended_view['parent']).to be_present
+        is_expected.to match({
+          'id' => category.id,
+          'name' => category.name,
+          'parent' => { 'id' => category.parent.id, 'name' => category.parent.name }
+
+        })
       end
 
       it 'renders parent basic information' do
-        expect(extended_view['parent']['id']).to be_present
-        expect(extended_view['parent']['id']).to be_present
-        expect(extended_view['parent']).not_to have_key('parent_id')
+        expect(subject['parent']).to match({
+          'id' => category.parent.id,
+          'name' => category.parent.name
+        })
       end
 
       it 'does not render a parent_id' do
-        expect(extended_view).not_to have_key('parent_id')
+        is_expected.not_to have_key('parent_id')
       end
     end
 
     context 'and no view is specified' do
-      let(:no_specific_view) { JSON.parse(CategoryBlueprint.render(category)) }
+      subject { JSON.parse(CategoryBlueprint.render(category)) }
 
       it 'rendes id, name and parent_id' do
-        expect(no_specific_view['name']).to be_present
-        expect(no_specific_view['id']).to be_present
-        expect(no_specific_view['parent_id']).to be_present
+        is_expected.to match({
+          'id' => category.id,
+          'name' => category.name,
+          'parent_id' => category.parent_id
+        })
       end
 
       it 'does not render a parent' do
-        expect(no_specific_view).not_to have_key('parent')
+        is_expected.not_to have_key('parent')
       end
     end
   end
